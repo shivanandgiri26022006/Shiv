77e257e138daf437c6021d17a41d7cacf239f7d6
Shivanand Giri <shivanandgiri026@gmail.com>
2025-09-29 14:18:31 +0530

    Add files via upload

---
+++ sum . c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+int A, B, sum;
+
+printf("Enter your first digit:");
+
+scanf("%d",&A);
+
+printf("Enter your second digit:");
+
+scanf("%d",&B);
+
+sum = A+B;
+
+printf("Total sum of A and B is:%d", sum);
+}
